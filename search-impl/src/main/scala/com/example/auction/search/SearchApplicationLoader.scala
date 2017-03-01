89f70798529cee52e93331677b8ea4d665c27d7b
kate <katejim@mail.ru>
2017-03-01 21:11:59 +0300

    Enable MetricsService

--- search-impl/src/main/scala/com/example/auction/search/SearchApplicationLoader.scala
+++ search-impl/src/main/scala/com/example/auction/search/SearchApplicationLoader.scala
@@ -6,17 +6,21 @@ import com.example.auction.search.api.SearchService
 import com.example.auction.search.impl.{BrokerEventConsumer, SearchServiceImpl}
 import com.example.elasticsearch.response.SearchResult
 import com.example.elasticsearch.{ElasticSearchIndexedStore, Elasticsearch}
+import com.lightbend.lagom.internal.client.CircuitBreakerMetricsProviderImpl
+import com.lightbend.lagom.internal.spi.CircuitBreakerMetricsProvider
 import com.lightbend.lagom.scaladsl.api.ServiceLocator
 import com.lightbend.lagom.scaladsl.api.ServiceLocator.NoServiceLocator
 import com.lightbend.lagom.scaladsl.broker.kafka.LagomKafkaClientComponents
 import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
+import com.lightbend.lagom.scaladsl.server.status.MetricsServiceComponents
 import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext, LagomApplicationLoader, LagomServer}
 import com.softwaremill.macwire._
 import play.api.libs.ws.ahc.AhcWSComponents
 
 abstract class SearchApplication(context: LagomApplicationContext) extends LagomApplication(context)
   with AhcWSComponents
-  with LagomKafkaClientComponents {
+  with LagomKafkaClientComponents
+  with MetricsServiceComponents {
 
   lazy val itemService = serviceClient.implement[ItemService]
   lazy val bidService = serviceClient.implement[BiddingService]
@@ -26,7 +30,8 @@ abstract class SearchApplication(context: LagomApplicationContext) extends Lagom
   lazy val indexedStore:IndexedStore[SearchResult] = wire[ElasticSearchIndexedStore]
 
   override lazy val lagomServer = LagomServer.forServices(
-    bindService[SearchService].to(wire[SearchServiceImpl])
+    bindService[SearchService].to(wire[SearchServiceImpl]),
+    metricsServiceBinding
   )
 
   wire[BrokerEventConsumer]
@@ -37,6 +42,9 @@ class SearchApplicationLoader extends LagomApplicationLoader {
   override def load(context: LagomApplicationContext) =
     new SearchApplication(context) {
       override def serviceLocator: ServiceLocator = NoServiceLocator
+
+      override def circuitBreakerMetricsProvider: CircuitBreakerMetricsProvider =
+        new CircuitBreakerMetricsProviderImpl(actorSystem)
     }
 
   override def loadDevMode(context: LagomApplicationContext) =

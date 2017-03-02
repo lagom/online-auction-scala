e2884560afb8db08a9129f1ae08250f6629d66d3
kate <katejim@mail.ru>
2017-03-02 23:23:09 +0300

    Set up circuit breaker with id - elasticsearch-circuitbreaker

--- search-impl/src/main/scala/com/example/elasticsearch/Elasticsearch.scala
+++ search-impl/src/main/scala/com/example/elasticsearch/Elasticsearch.scala
@@ -5,7 +5,7 @@ import java.util.UUID
 import akka.Done
 import com.lightbend.lagom.scaladsl.api.Service._
 import com.lightbend.lagom.scaladsl.api.transport.Method
-import com.lightbend.lagom.scaladsl.api.{Descriptor, Service, ServiceCall}
+import com.lightbend.lagom.scaladsl.api.{CircuitBreaker, Descriptor, Service, ServiceCall}
 import com.example.elasticsearch.request._
 import com.example.elasticsearch.response._
 
@@ -13,8 +13,10 @@ trait Elasticsearch extends Service {
   override def descriptor: Descriptor =
     named("elastic-search")
       .withCalls(
-        restCall(Method.GET, "/:index/items/_search", search _),
+        restCall(Method.GET, "/:index/items/_search", search _)
+          .withCircuitBreaker(CircuitBreaker.identifiedBy("elasticsearch-circuitbreaker")),
         restCall(Method.POST, "/:index/items/:id/_update", updateIndex _)
+          .withCircuitBreaker(CircuitBreaker.identifiedBy("elasticsearch-circuitbreaker"))
       ).withAutoAcl(true)
 
   def search(index: String): ServiceCall[QueryRoot, SearchResult]
